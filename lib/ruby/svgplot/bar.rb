c3e5680c239d586e6d63bcebca399136c9ababa3
slowness pc <slma0x02@gmail.com>
2013-07-15 20:21:33 +0400

    palettes

--- lib/ruby/svgplot/bar.rb
+++ lib/ruby/svgplot/bar.rb
@@ -6,8 +6,9 @@ class Svgplot::BarChart < Svgplot::Plot
 
 	def plot(args)
 		style = args[:style] ||= DefaultStyle
+		palette = style["fill"].is_a?(Array) ? style["fill"].dup : [style["fill"]]
 		raise ArgumentError, "inappropriate data type" if args[:data].nil?
-		opts = { :barwidth => (@param[:w] - 2*@param[:span])/args[:data].count, :show_captions => true }
+		opts = { :barwidth => (@param[:w] - 2*@param[:span])/(args[:data].count+2), :show_captions => true }
 		opts.merge! args[:options] unless args[:options].nil?
 		st = DefaultStyle.merge args[:style]
 		chart_data = args[:data]
@@ -20,10 +21,17 @@ class Svgplot::BarChart < Svgplot::Plot
 
 		i = 0
 		chart_data.each do |caption, val|
-			text (i+0.1) * barw, h/2, caption.to_s if opts[:show_captions]
+			text s+ (i+0.9)*step , h/2, caption.to_s if opts[:show_captions]
 			if val.class == Array
-				#some rocket science
+				tot = val.reduce :+
+				cur = 0
+				val.each_with_index do |v,j|
+					style["fill"] = pick_color j, palette
+					rectangle s+(i+1)*step - barw/2, h - pcnt*(tot - cur), barw, pcnt*v, style
+					cur += v
+				end
 			else
+				style["fill"] = pick_color i,palette
 				rectangle s+(i+1)*step - barw/2, h - pcnt*val, barw, pcnt*val, style
 			end
 			i+=1
--- test/bar.rb
+++ test/bar.rb
@@ -3,22 +3,32 @@ require 'ruby/svgplot'
 p = Svgplot::BarChart.new(:w => 800, :h => 500) do
 	plot(
 		:desc => "Ohh, some fancy data",
-		:data => {
-			"group_one" => 10,
-			"group_two" => 90,
-			"group_three" => 55
+		:data => { 
+			"1" => 6, "4" => 6, "2" => 7, "4" => 9, "5" => 11,
+			"3" => 6, "x" => 6, "g2" => 7, "a4" => 9, "q5" => 11
 		},
 		:style => {
 			"fill-opacity" => "0.4",
-			"fill" => "green",
+			"fill" => ["green"],
 			"stroke" => "black"
+		}
+	)
+end
+p2 = Svgplot::BarChart.new(:w => 800, :h => 500) do
+	plot(
+		:data => {
+			"cap1" => [40,5,6],
+			"cap2" => [6,7,8],
+			"cap3" => [1,2]
 		},
-		:options => {
-			:show_captions => false,
-			:barwidth => 100
+		:style => {
+			"fill" => ["green", "black"]
 		}
 	)
 end
 File.open("test/bar.svg","w") do |f|
 	f.puts p.output
+end
+File.open("test/bar2.svg","w") do |f|
+	f.puts p2.output
 end
